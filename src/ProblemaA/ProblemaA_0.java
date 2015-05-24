0686b501c7e95982541cf9cb8dbf342833740e3d
mvanegas10 <m.vanegas10@uniandes.edu.co>
2015-05-24 00:33:24 -0500

    :rabbit: :panda_face: :zzz:

--- src/ProblemaA/ProblemaA_0.java
+++ src/ProblemaA/ProblemaA_0.java
@@ -12,6 +12,7 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Stack;
 
 public class ProblemaA_0 {
 
@@ -40,8 +41,22 @@ public class ProblemaA_0 {
 			}
 			
 			int [] resp = new int [2];
-			resp = propuesta(foto);
-			System.out.println(resp[0] + " " + resp[1]);
+			
+			int [] prueba = new int [7];
+			prueba[0] = 10;
+			prueba[1] = 40;
+			prueba[2] = 30;
+			prueba[3] = 70;
+			prueba[4] = 10;
+			prueba[5] = 30;
+			prueba[6] = 60;
+			
+			System.out.println(rectanguloMaximo(prueba));
+			
+			
+//			resp = propuestaTres(foto);
+//
+//			System.out.println(resp[0] + " " + resp[1]);
 		}
 	}
 
@@ -97,10 +112,118 @@ public class ProblemaA_0 {
 		return resp;
 	}
 	
-	//Solución propuesta
-	public static int[] propuesta(boolean [][] foto){
+	public static int[] propuestaTres(boolean [][] foto){
 		int cmax = 0;
 		int rmax = 0;
+		int m = foto.length;
+		int n = foto[0].length;
+		int i = m - 1;
+		int j = n - 1;
+		boolean nuevo = true;
+		int [][] matriz = new int [m][n];
+		int [][] maxVertical = new int [m][n];
+		while (i >= 0) {
+			while (j >= 0) {
+				if (foto[i][j]){
+					if ( i != m - 1 && j != n - 1){
+						
+						//Matriz para solución de cuadrados
+						
+						int uno = matriz[i+1][j];
+						int dos = matriz[i][j+1];
+						int tres = matriz[i+1][j+1];
+						matriz[i][j] = 1 + Math.min((Integer) Math.min(uno, dos), tres);
+						
+					}
+					else
+						matriz[i][j] = 1;
+					
+					//Matriz para solución de rectángulos
+					
+					if (i < m - 1){
+						if (maxVertical[i + 1][j] > 0) maxVertical[i][j] = maxVertical[i + 1][j] - 1;
+						else nuevo = true;
+					}
+					
+					if (nuevo){
+						int k = i;
+						int suma = 0;
+						while (k >= 0) {
+							if(foto[k][j]) suma++;
+							else k = 0;
+							k--;
+						}
+						maxVertical[i][j] = suma;
+					}
+				}
+				else{
+					matriz[i][j] = 0;
+					maxVertical[i][j] = 0;
+				}
+				cmax = Math.max(matriz[i][j], cmax);
+				j--;
+			}
+			i--;
+			j = n - 1;
+		}
+		
+		//Encuentra rectángulo con área máxima
+		
+		i = m - 1;
+		while (i >= 0) {
+			rmax = Math.max(rmax, rectanguloMaximo(maxVertical[i]));						
+			i--;
+		}
+		
+		int [] resp = new int [2];
+		resp[0] = cmax*cmax;
+		resp[1] = rmax;
+		return resp;
+	}
+	
+	public static int rectanguloMaximo(int [] arreglo){
+		int maxArea = 0;
+		Stack<Integer> stk = new Stack<Integer>();
+		stk.push(0);
+		for (int i = 1; i < arreglo.length; i++) {
+			int actual = arreglo[i];
+			
+			while(!stk.empty()){
+				int tope = stk.peek();
+				
+				if (actual > arreglo[tope]){
+					stk.push(i);
+					break;
+				}
+				
+				tope = stk.pop();
+				
+				int izquierdo = (stk.empty())? 0: stk.peek();
+				int derecho = (arreglo[tope] == actual)? i: i - 1;
+				
+				int area = (derecho - izquierdo) * arreglo[tope];
+				maxArea = (area > maxArea)? area: maxArea;
+			}
+			
+			if (stk.empty()) stk.push(i);
+			}
+		
+		int derecho = arreglo.length;
+		while( !stk.empty() ){
+			int tope = stk.pop();
+			int izquierdo = (stk.empty())? 0: stk.peek();
+			int area = (derecho - izquierdo) * arreglo[tope];
+			maxArea = (area > maxArea)? area: maxArea;
+		}
+		return maxArea;
+	}
+	
+	//Solución propuesta
+	public static int[] propuesta(boolean [][] foto){
+		int iVer = 0;
+		int jVer = 0;
+		int iHor = 0;
+		int jHor = 0;
 		int m = foto.length;
 		int n = foto[0].length;
 		boolean nuevo = true;
@@ -108,49 +231,120 @@ public class ProblemaA_0 {
 		int [][] maxVertical = new int [m][n];
 		for(int i=0; i != m; i++){
 			for (int j=0; j != n; j++){
-				int maximo = 0;
 				if (foto[i][j]){
 					nuevo = !(i > 0 || j > 0);
 					if (!nuevo){
 						
-						maxHorizontal[i][j] = (j > 0 && maxHorizontal[i][j - 1] != 0)?(maxVertical[i][j - 1] - 1):1;
 						if (i > 0){
-							if (maxVertical[i - 1][j] != 0)
+							if (maxVertical[i - 1][j] > 0)
 								maxVertical[i][j] = maxVertical[i - 1][j] - 1;
 							else
 								nuevo = true;
 						}
 						if (j > 0){
-							if (maxHorizontal[i][j - 1] != 0)
-								maxHorizontal[i][j] = maxVertical[i][j - 1] - 1;
+							if (maxHorizontal[i][j - 1] > 0)
+								maxHorizontal[i][j] = maxHorizontal[i][j - 1] - 1;
 							else
 								nuevo = true;
 						}
 					}
 					if (nuevo){
-						int suma = 1;
+						int suma = 0;
 						for (int k = i; k != m; k++){
 							if(foto[k][j])
 								suma++;
 							else
-								k = m;
+								k = m-1;
 						}
 						maxVertical[i][j] = suma;
-						suma = 1;
+						if(suma > maxVertical[iVer][jVer]){
+							maxVertical[iVer][jVer] = suma;
+							iVer = i;
+							jVer = j;
+						}
+						suma = 0;
 						for (int k = j; k != n; k++){
 							if(foto[i][k])
 								suma++;
 							else
-								k = n;
+								k = n-1;
 						}
 						maxHorizontal[i][j] = suma;
+						if(suma > maxHorizontal[iHor][jHor]){
+							maxHorizontal[iHor][jHor] = suma;
+							iHor = i;
+							jHor = j;
+						}
 					}
 				}
 			}
 		}
+		if (maxHorizontal[iHor][jHor] > maxVertical[iVer][jVer]){
+			
+		}
 		int [] resp = new int [2];
-		resp[0] = cmax;
-		resp[1] = rmax;
 		return resp;
 	}
+	
+	
+	public static int[] propuestaDos(boolean [][] foto){
+		
+		int m = foto.length;
+		int n = foto[0].length;
+		int i = 0;
+		int j = 0;
+		int iInicial = 0;
+		int jInicial = 0;
+		int iFinal = iInicial;
+		int jFinal = - 1;
+		int maximo = 0;
+		int cuenta = 0;
+		int stop = 0;
+		while (i < iFinal + 1){
+			if (foto[i][j]){
+				cuenta++;
+			
+				if (j > jInicial ){
+					i ++;
+					j --;
+				}
+				else{
+					j = i + 1;
+					i = 0;
+					jFinal = j;
+					iFinal ++;
+				}	
+			}
+			else{
+				 stop --;
+				 if (stop == - 2){
+					 maximo = Math.max(cuenta, maximo);
+					 stop = 0;
+					 iFinal = iInicial;
+					 jFinal = jInicial;
+					 if (j > jInicial){
+						jInicial = iFinal + 1;
+						iInicial = 0;
+					 }
+					 else {
+						iInicial = i ++;
+						jInicial = j --;
+					 }
+				 }
+				 if (j > jInicial){
+					j = iFinal + 1;
+					i = 0;
+				 }
+				 else {
+					i = i ++;
+					j = j --;
+				 }
+			}
+		}
+		int [] resp = new int [2];
+		return resp;
+	}
+	
+	
 }
+
