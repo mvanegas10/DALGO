d7f8fc6f7af8953c784fe5a3996a33bb8147d319
RamirezAmayaS <s.ramirez34@uniandes.edu.co>
2015-05-24 21:53:16 -0500

    Punto1

--- src/ProblemaA/ProblemaA_0.java
+++ src/ProblemaA/ProblemaA_0.java
@@ -1,6 +1,6 @@
 package ProblemaA;
 // DAlgo 2015-10
-// Problema A    : Solución ingenua
+// Problema A    : Soluciï¿½n ingenua
 // Grupo         : 00
 // Autor         : Rodrigo Cardoso
 
@@ -30,7 +30,7 @@ public class ProblemaA_0 {
 			data = lineain.split(" ");
 			M = Integer.parseInt(data[0]);
 			N = Integer.parseInt(data[1]);
-			if (M==0 && N ==0) break;				// terminación de la entrada
+			if (M==0 && N ==0) break;				// terminaciï¿½n de la entrada
 			boolean [][] foto = new boolean [M][N];
 			for (int i=0; i!=M; i++){
 				lineain = br.readLine();
@@ -45,7 +45,7 @@ public class ProblemaA_0 {
 			}
 			
 			int [] resp = new int [2];
-			resp = propuesta(foto);
+			resp = solve(foto);
 			System.out.println(resp[0] + " " + resp[1]);
 		}
 		
@@ -53,7 +53,7 @@ public class ProblemaA_0 {
 		System.out.println(fecha.getHours() + " H " + fecha.getMinutes() + " MIN " + fecha.getSeconds() + " SECS ");
 	}
 
-	//Solución ingenua
+	//Soluciï¿½n ingenua
 	public static int[] solve(boolean [][] foto){
 		int cmax = 0;
 		int rmax = 0;
@@ -94,7 +94,7 @@ public class ProblemaA_0 {
 						}
 					}
 				}
-				//Actualiza el máximo
+				//Actualiza el mï¿½ximo
 				rmax = Math.max(rmax, rmaxij);
 				cmax = Math.max(cmax, cmaxij);
 			}
@@ -103,148 +103,6 @@ public class ProblemaA_0 {
 		resp[0] = cmax;
 		resp[1] = rmax;
 		return resp;
-	}
-	
-	
-	/**
-	 * Propuesta de mejora, retorna los valores del área del rectángulo y cuadrado más grande de una foto
-	 * @param foto Matriz booleana
-	 * @return	área del rectángulo y cuadrado más grande de una foto
-	 */
-	public static int[] propuesta (boolean [][] foto){
-		int cmax = 0;
-		int rmax = 0;
-		int m = foto.length;
-		int n = foto[0].length;
-		int i = m - 1;
-		int j = n - 1;
-		boolean nuevo = true;
-		int [][] matriz = new int [m][n];
-		int [][] maxVertical = new int [m][n + 1];
-		
-		while (i >= 0) {
-			while (j >= 0) {
-				
-				//	Hay un uno
-				
-				if (foto[i][j]){
-					
-					//	Es interno
-					
-					if ( i != m - 1 && j != n - 1){
-						
-						//	Matriz para solución de cuadrados
-						
-						int uno = matriz[i+1][j];
-						int dos = matriz[i][j+1];
-						int tres = matriz[i+1][j+1];
-						matriz[i][j] = 1 + Math.min((Integer) Math.min(uno, dos), tres);
-						
-					}
-					
-					//	Es un borde
-					
-					else
-						matriz[i][j] = 1;
-					
-					//	Matriz para solución de rectángulos
-					
-					//	Es interno
-					
-					if (i < m - 1){
-						if (maxVertical[i + 1][j + 1] > 0) maxVertical[i][j + 1] = maxVertical[i + 1][j + 1] - 1;
-						else nuevo = true;
-					}
-					
-					//	No se ha calculado el histograma
-					
-					if (nuevo){
-						int k = i;
-						int suma = 0;
-						while (k >= 0) {
-							if(foto[k][j]) suma++;
-							else k = 0;
-							k--;
-						}
-						maxVertical[i][j + 1] = suma;
-					}
-				}
-				
-				//	Es un cero
-				
-				else{
-					matriz[i][j] = 0;
-					maxVertical[i][j + 1] = 0;
-				}
-				cmax = Math.max(matriz[i][j], cmax);
-				j--;
-			}
-			i--;
-			j = n - 1;
-		}
-		
-		//Encuentra rectángulo con área máxima
-		
-		i = m - 1;
-		while (i >= 0) {
-			rmax = Math.max(rmax, rectanguloMaximo(maxVertical[i]));						
-			i--;
-		}
-		
-		int [] resp = new int [2];
-		resp[0] = cmax*cmax;
-		resp[1] = rmax;
-		return resp;
-	}
-	
-
-	/**
-	 * Calucula el área del rectángulo más grande dado un histograma
-	 * @param arreglo El arreglo que contiene los valores del histograma
-	 * @return El área del rectángulo máximo dado un histograma
-	 */
-	public static int rectanguloMaximo(int [] arreglo){
-		int maxArea = 0;
-		Stack<Integer> stk = new Stack<Integer>();
-		stk.push(0);
-		
-		for (int i = 1; i < arreglo.length; i++) {
-			int actual = arreglo[i];
-			
-			while(!stk.empty()){
-				int tope = stk.peek();
-				
-				//	Verifica si los valores son crecientes
-				
-				if (actual > arreglo[tope]){
-					stk.push(i);
-					break;
-				}
-				
-				//	Elimina el más grande de la pila
-				
-				tope = stk.pop();
-				
-				int izquierdo = (stk.empty())? 0: stk.peek() + 1;
-				int derecho = (arreglo[tope] == actual)? i + 1: i;
-				
-				int area = (derecho - izquierdo) * arreglo[tope];
-				maxArea = (area > maxArea)? area: maxArea;
-			}
-			
-			if (stk.empty()) stk.push(i);
-		}
-		
-		//	Vacía la pila y recalcula el área máxima
-		
-		int derecho = arreglo.length;
-		while( !stk.empty() ){
-			int tope = stk.pop();
-			int izquierdo = (stk.empty())? 0: stk.peek() + 1;
-			int area = (derecho - izquierdo) * arreglo[tope];
-			maxArea = (area > maxArea)? area: maxArea;
-		}
-		return maxArea;
 	}
 }
 
